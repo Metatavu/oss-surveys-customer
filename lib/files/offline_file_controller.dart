ae7601cf686904140a210a323e07565474f4970b
Ville Juutila <ville.juutila@gmail.com>
2023-04-06 21:58:51 +0300

    Added OfflineFileController docs.

--- lib/files/offline_file_controller.dart
+++ lib/files/offline_file_controller.dart
@@ -1,50 +1,65 @@
+import "dart:async";
 import "dart:convert";
 import 'dart:io';
 import "package:crypto/crypto.dart";
 import "package:oss_surveys_customer/main.dart";
 import "package:path_provider/path_provider.dart";
 import "package:path/path.dart" as p;
+import "package:typed_data/typed_data.dart";
 
+/// Offline File Controller class
 class OfflineFileController {
   HttpClient httpClient = HttpClient();
-  Future<Directory> getDownloadsDir() => getApplicationSupportDirectory();
 
-  /// TODO: ADD DOCS
-  Future<File?> getOfflineFile(String url, { download = true }) async {    
+  /// Gets offlined file by [url]
+  ///
+  /// Downloads file by default, can be changed by setting [download] to false.
+  Future<File?> getOfflineFile(String url, { download = true }) async {
     try {
       if (download) {
         return _download(url);
       } else {
-        return File(p.join((await getApplicationSupportDirectory()).path, _getOfflineFileName(url)));
+        File offlinedFile = File(p.join((await _getImagesDirectoryPath()), _getOfflineFileName(url)));
+        if (await offlinedFile.exists()) {
+          return offlinedFile;
+        }
+        return null;
       }
     } catch (e) {
       logger.shout("Couldn't download file from $url");
       return null;
     }
   }
-  
-  /// TODO: ADD DOCS
+
+  /// Downloads file from [url]
+  ///
+  /// If the same file is already downloaded, applies its ETag to the request headers to not download it again in case it is not modified.
+  /// In case of file not modified, returns the already offlined file.
   Future<File?> _download(String url) async {
     Uri uri = Uri.parse(url);
     String fileName = _getOfflineFileName(url);
-    File file = File(p.join((await getApplicationSupportDirectory()).path, fileName));
+    File? metaFile = await _getDirectoryFileByNameAndExt(await _getImagesDirectoryPath(), fileName, ".meta");
+
     HttpClientRequest request = await httpClient.getUrl(uri);
-    String? eTag = await readFileETag(file);
-    
-    if (eTag != null) {
-      request.headers.add("If-None-Match", eTag, preserveHeaderCase: true);  
+
+    if (metaFile != null) {
+      String? eTag = (await _readMetaFile(metaFile))?.eTag;
+
+      if (eTag != null) {
+        request.headers.add("If-None-Match", eTag, preserveHeaderCase: true);
+      }
     }
-    
+
     HttpClientResponse response = await request.close();
-    
+
     switch (response.statusCode) {
       case 200: {
         logger.info("Downloading file $url...");
-        return await _handleSuccessfulDownload(fileName, response);
+        return await _handleNewFile(fileName, response);
       }
       case 304: {
         logger.info("File not changed. Using offlined file $url");
-        return file;
+        return await _getDirectoryFileByNameAndExt(await _getImagesDirectoryPath(), fileName, ".jpeg");
       }
       default: {
         logger.shout("Failed to download file $url, ${response.statusCode}, ${response.reasonPhrase}");
@@ -53,98 +68,130 @@ class OfflineFileController {
     }
   }
 
-  /// TODO: ADD DOCS
+  /// Gets offline file name based on hash of [url] and name of the file.
   String _getOfflineFileName(String url) {
-    String urlHash = _calculateMd5(url);
-    String fileExtension = url.substring(url.lastIndexOf("."));
-    
-    return urlHash + fileExtension;
+    String urlHash = md5.convert(utf8.encode(url)).toString();
+    String fileName = url.substring(url.lastIndexOf("/") + 1);
+
+    return "$urlHash-$fileName";
   }
-  
-  /// TODO: ADD DOCS
-  String _calculateMd5(String string) => md5.convert(utf8.encode(string)).toString();
-  
-  /// TODO: ADD DOCS
-  Future<String?> readFileETag(File file) async {
-    FileMeta? metaFile = await _readFileMeta(file);
-    
-    return metaFile?.eTag;
-  }
-  
-  /// TODO: ADD DOCS
-  Future<FileMeta?> _readFileMeta(File file) async {
+
+  /// Reads given .meta [file].
+  /// 
+  /// Returns null if given file or metafile does not exist.
+  Future<MetaFile?> _readMetaFile(File file) async {
     if (!await file.exists()) {
       return null;
     }
     
-    File metaFile = File(await _getMetafileName(file));
-    
+    File metaFile = File(await _getMetaFilePath(file));
+
     if (!await metaFile.exists()) {
       return null;
     }
     
-    return FileMeta.fromJson(jsonDecode(await metaFile.readAsString()));
+    return MetaFile.fromJson(jsonDecode(await metaFile.readAsString()));
   }
-  
-  /// TODO: ADD DOCS
-  Future<void> _writeFileMeta(File file, String? eTag) async {
-    File metaFile = File(await _getMetafileName(file));
-    
+
+  /// Writes .meta file for [file] and [eTag] as JSON with its content.
+  Future<void> _writeMetaFile(File file, String? eTag) async {
+    File metaFile = File(await _getMetaFilePath(file));
+
     if (await metaFile.exists()) {
       metaFile.delete();
     }
-    
-    (await metaFile.create(recursive: true)).writeAsString(
-      FileMeta.fromJson(jsonDecode(eTag ?? "")).toJson().toString()
-    );
+
+    await metaFile.writeAsString('{"eTag":${eTag != null ? "$eTag" : null}}');
   }
-  
-  /// TODO: ADD DOCS
-  Future<String> _getMetafileName(File file) async => "${(await getApplicationSupportDirectory()).path}/${p.basename(file.path)}.meta";
-  
-  /// TODO: ADD DOCS
-  Future<File?> _handleSuccessfulDownload(String fileName, HttpClientResponse response) async {
-    String? eTag = response.headers.value("If-None-Match");
-    String stringData = response.transform(utf8.decoder).toString();
+
+  /// Gets path to given [file]s .meta file
+  Future<String> _getMetaFilePath(File file) async => "${(await _getImagesDirectoryPath())}/${p.withoutExtension(p.basename(file.path))}.meta";
+
+  /// Handles creating of new file with [fileName].
+  /// 
+  /// ETag and file content is parsed from [response].
+  Future<File?> _handleNewFile(String fileName, HttpClientResponse response) async {
+    String? eTag = response.headers.value("ETag");
     File existingFile = File(fileName);
-    
-    if (stringData.isEmpty) {
-      return null;
-    }
-    
-    String filePartName = "${(await getApplicationSupportDirectory()).path}/$fileName.part";
-    print("FILEPARTNAME: $filePartName");
+    String? fileExtension = _getFileExtFromHeaders(response);
+    String filePartName = "${(await _getImagesDirectoryPath())}/$fileName.part";
     File filePart = File(filePartName);
-    
+    String newFileName = filePart.path.replaceAll(".part", ".$fileExtension");
+
     if (await filePart.exists()) {
       await filePart.delete();
     }
-    
-    (await filePart.create(recursive: true)).writeAsBytes(stringData.codeUnits);
-    
+
+    await filePart.writeAsBytes(await _readResponseToBytes(response));
+
     if (await existingFile.exists()) {
-      existingFile.delete();
+      await existingFile.delete();
     }
     
-    filePart.rename(fileName);
-    await _writeFileMeta(existingFile, eTag);
+    await filePart.rename(newFileName);
+    await _writeMetaFile(existingFile, eTag);
     logger.info("Downloaded $fileName!");
-    
+
     return existingFile;
+  }
+
+  /// Reads [response] to bytes and awaits for it to be ready.
+  Future<Int8Buffer> _readResponseToBytes(HttpClientResponse response) {
+    final completer = Completer<Int8Buffer>();
+    final byteBuffer = Int8Buffer();
+    response.listen((data) => byteBuffer.addAll(data), onDone: () => completer.complete(byteBuffer));
+
+    return completer.future;
+  }
+
+  /// Returns path to offlined images directory as string.
+  Future<String> _getImagesDirectoryPath() async {
+    return (await Directory("${(await getApplicationSupportDirectory()).path}/images").create()).path;
+  }
+
+  /// Searches directory at [directoryPath] for file with [fileName] and extension [fileExt].
+  /// 
+  /// Returns found file or null, if specified file was not found.
+  Future<File?> _getDirectoryFileByNameAndExt(String directoryPath, String fileName, String fileExt) async {
+    try {
+      return File((await Directory(directoryPath)
+        .list()
+        .firstWhere((fileSystemEntity) => p.basenameWithoutExtension(fileSystemEntity.path) == fileName && p.extension(fileSystemEntity.path) == fileExt)).path
+    );
+    } catch (e) {
+      logger.warning("Couldn't find File $fileName$fileExt in $directoryPath");
+      return null;
+    }
+  }
+
+  /// Gets desired file extension from [response] Content-Type header.
+  /// 
+  /// Returns null if Content-Type header is not present.
+  String? _getFileExtFromHeaders(HttpClientResponse response) {
+    String? contentTypeHeader = response.headers.value("Content-Type");
+
+    if (contentTypeHeader == null) {
+      return null;
+    }
+
+    return contentTypeHeader.substring(contentTypeHeader.lastIndexOf("/") + 1);
   }
 }
 
-/// TODO: ADD DOCS
-class FileMeta {
-  
-  FileMeta(this.eTag);
-  
-  String eTag;
-  
-  /// TODO: ADD DOCS
-  factory FileMeta.fromJson(Map<String, dynamic> json) => FileMeta(json["eTag"] as String);
-  
-  /// TODO: ADD DOCS
+/// MetaFile class
+/// 
+/// Describes content of .meta files associated with offlined files.
+/// Contains files ETag to check if online file has changed since last download.
+class MetaFile {
+
+  MetaFile(this.eTag);
+
+  String? eTag;
+
+  /// Factory method for constructing MetaFile instances from [json].
+  factory MetaFile.fromJson(Map<String, dynamic> json) => MetaFile(json["eTag"] as String?);
+
+  /// Returns MetaFile as JSON (map).
   Map<String, dynamic> toJson() => {
     "eTag": eTag
   };
